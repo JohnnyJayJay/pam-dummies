76b6fb67488763e5301044d63238dfd12ee9036d
JohnnyJayJay <johnnyjayjay02@gmail.com>
2023-03-14 15:40:31 +0100

    Add delaying pam module

---
+++ delayed.c
@@ -0,0 +1,26 @@
+#include <security/_pam_types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <security/pam_modules.h>
+#include <security/pam_ext.h>
+
+PAM_EXTERN int pam_sm_authenticate(
+    __attribute__ ((unused)) pam_handle_t *pamh,
+    __attribute__ ((unused)) int flags,
+    int argc,
+    const char **argv
+) {
+    int seconds = 5;
+    if (argc > 0) {
+        seconds = atoi(argv[0]);
+        if (seconds <= 0) {
+            return PAM_SYSTEM_ERR;
+        }
+    }
+    printf("Authenticating you after %i seconds...", seconds);
+    sleep(seconds);
+    return PAM_SUCCESS;
+}
